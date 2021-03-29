b0ead5319337ac376b085c06a1e751d254e3696e
ITI <dinaelshabassy@gmail.com>
2021-03-29 23:50:06 +0200

    navigation drawer is added + display user info

--- app/src/main/java/ahmed/adel/sleeem/clowyy/triptracker/MainActivity.java
+++ app/src/main/java/ahmed/adel/sleeem/clowyy/triptracker/MainActivity.java
@@ -1,57 +1,51 @@
 package ahmed.adel.sleeem.clowyy.triptracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.badge.BadgeUtils;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+import com.bumptech.glide.Glide;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import java.util.HashMap;
-import java.util.UUID;
-
-import ahmed.adel.sleeem.clowyy.triptracker.database.model.Trip;
-import ahmed.adel.sleeem.clowyy.triptracker.database.model.TripDao;
-import ahmed.adel.sleeem.clowyy.triptracker.database.model.TripDatabase;
-import ahmed.adel.sleeem.clowyy.triptracker.managers.DialogAlert;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppBarConfiguration mAppBarConfiguration;
     // Session Manager Class
     SessionManager session;
-    Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        logout = findViewById(R.id.logoutBtn);
-
         //check shared preferences and user login status
 
         // Session class instance
         session = new SessionManager(getApplicationContext());
         Toast.makeText(getApplicationContext(), "User Login Status: " + session.isLoggedIn(), Toast.LENGTH_LONG).show();
 
-
-
-
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-
-        Intent intent = new Intent(getApplicationContext(), DialogAlert.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(getBaseContext(),1,intent,0);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            alarmManager.setExact(AlarmManager.RTC_WAKEUP,10000,pendingIntent);
-            
-        }
 
         /**
          * Call this function whenever you want to check user login
@@ -60,33 +54,125 @@ public class MainActivity extends AppCompatActivity {
          * */
         session.checkLogin();
 
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //initialize trip activity intent
+                Intent trip  = new Intent(MainActivity.this,TripActivity.class);
+                startActivity(trip);
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_upcomingTrips, R.id.nav_history)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+
+        //components of header view
+        View headerView = navigationView.getHeaderView(0);
+        ImageView userImage = headerView.findViewById(R.id.userImage);
         // get user data from session
         HashMap<String, String> user = session.getUserDetails();
+        String imageUrl = user.get(SessionManager.KEY_ImageURL);
+        switch(imageUrl){
+            case "":
+                break;
+            default:
+                Glide.with(getApplicationContext()).load(imageUrl).into(userImage);
+                break;
+        }
 
-        // email
         String email = user.get(SessionManager.KEY_EMAIL);
+        String username = user.get(SessionManager.KEY_NAME);
+        TextView userName = headerView.findViewById(R.id.userName);
+        userName.setText(username);
+        TextView userEmail = headerView.findViewById(R.id.userEmail);
+        userEmail.setText(email);
 
-//        TripDatabase tripDatabase = TripDatabase.getInstance(getApplicationContext());
-//        TripDao tripDao = tripDatabase.getTripDao();
-//
-//        Trip trip = new Trip("A","new trip","B",true,
-//                'D',"eat drink  swim",email,"2021-3-26","10:48",
-//                UUID.nameUUIDFromBytes("ahmed.png".getBytes()).toString(),false);
-//
-//
-//
-//        tripDao.insertTrip(trip);
-        Toast.makeText(getApplicationContext(),"Email : " + email, Toast.LENGTH_SHORT).show();
 
-        logout.setOnClickListener(new View.OnClickListener() {
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
             @Override
-            public void onClick(View v) {
-                // Clear the session data
-                // This will clear all session data and
-                // redirect user to LoginActivity
-                session.logoutUser();
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+
+                int menuId = destination.getId();
+                switch (menuId){
+                    case R.id.nav_history:
+                        fab.hide();
+                        break;
+                    default:
+                        fab.show();
+                        break;
+                }
             }
         });
 
+        navigationView.getMenu().findItem(R.id.nav_logout).setOnMenuItemClickListener(menuItem -> {
+            Toast.makeText(MainActivity.this, "logout is clicked", Toast.LENGTH_SHORT).show();
+            logout(MainActivity.this);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        });
+
+        navigationView.getMenu().findItem(R.id.nav_sync).setOnMenuItemClickListener(menuItem -> {
+            Toast.makeText(MainActivity.this, "Sync is clicked", Toast.LENGTH_SHORT).show();
+            //add sync function
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        });
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    public static void logout(final Activity activity)
+    {
+
+
+        //initialize alert dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        //set title
+        builder.setTitle("Logout");
+        //set message
+        builder.setMessage("Are you sure you want to logout ?");
+        //positive yes button
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //call method logout in session class
+                //session.logoutUser();
+            }
+        });
+        //negative no button
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //dismiss dialog
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
 }
--- app/src/main/java/ahmed/adel/sleeem/clowyy/triptracker/SplashScreenActivity.java
+++ app/src/main/java/ahmed/adel/sleeem/clowyy/triptracker/SplashScreenActivity.java
@@ -2,19 +2,13 @@ package ahmed.adel.sleeem.clowyy.triptracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import ahmed.adel.sleeem.clowyy.triptracker.managers.DialogAlert;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -27,9 +21,9 @@ public class SplashScreenActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
 
-        logoImage=findViewById(R.id.imageView);
+        logoImage=findViewById(R.id.userImage);
 
-        sloganTxt = findViewById(R.id.textView);
+        sloganTxt = findViewById(R.id.userEmail);
         logoImage.setAnimation(AnimationUtils.loadAnimation(getBaseContext(),R.anim.side_anim));
         sloganTxt.setAnimation(AnimationUtils.loadAnimation(getBaseContext(),R.anim.bottom_anim));
 
